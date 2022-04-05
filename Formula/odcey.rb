1f0f985d986e02a77926745c53c1ce75a8755967
ComdivByZero <ComdivByZero@yandex.ua>
2022-04-05 04:52:35 +0300

    Добавлена формула для odcey

---
+++ Formula/odcey.rb
@@ -0,0 +1,18 @@
+class Odcey < Formula
+  desc "The converter from BlackBox Component Builder .odc format to plain text"
+  homepage "https://github.com/Vostok-space/odcey"
+  url "https://github.com/Vostok-space/odcey/archive/refs/tags/v0.d.3.tar.gz"
+  sha256 "d6a38662d8e187660b2243de9fcab23eed36d0fb2add7edc2746ec4d4efe8200"
+  license "Apache-2.0"
+
+  depends_on "vostok" => [:build]
+
+  def install
+    system "ost", "to-bin", "odcey.Cli", "odcey", "-m", ".", "-cc", "cc -s -O1 -flto"
+    system "cp", "odcey", "#{bin}"
+  end
+
+  test do
+    system "#{bin}/odcey", "help"
+  end
+end
