1a9c30e72cf3208355b2ec663cd478635ae8c1ea
ComdivByZero <ComdivByZero@yandex.ru>
2025-03-22 11:11:55 +0200

    homepage http->https

--- Formula/vostok.rb
+++ Formula/vostok.rb
@@ -1,6 +1,6 @@
 class Vostok < Formula
   desc "Oberon-07 translator"
-  homepage "http://vostok.oberon.org"
+  homepage "https://vostok.oberon.org"
   url "https://github.com/Vostok-space/vostok/archive/refs/tags/lib-v0.0.10_ost-v0.0.11.tar.gz"
   sha256 "a5bca9b4484f6879e92c7637557eb2c5f1d9a20b722c0ae6cc21a451c56611f3"
   license "LGPL-3.0-only"
