26a3f8283c3dbe2347010f0b0c738b3490e608f7
ComdivByZero <ComdivByZero@yandex.ru>
2026-02-10 00:58:05 +0200

    odcey v0.3.2

--- Formula/odcey.rb
+++ Formula/odcey.rb
@@ -1,15 +1,15 @@
 class Odcey < Formula
   desc      "Converter from BlackBox Component Builder .odc format to plain text"
   homepage  "https://github.com/Vostok-space/odcey"
-  url       "https://github.com/Vostok-space/odcey/archive/refs/tags/v0.3.tar.gz"
-  sha256    "46dd12b4642682039f9472e72e87b2d53c10085113d947c7b3d1815c70631dbb"
+  url       "https://github.com/Vostok-space/odcey/archive/refs/tags/v0.3.2.tar.gz"
+  sha256    "25fc432e1c95631d5d119aac7dfc078fe82ccc5699b550affd28a1ed635a29e8"
   license   "Apache-2.0"
 
   depends_on "vostok-space/oberon/vostok" => :build
 
   def install
     mkdir_p bin
-    system "ost", "to-bin", "odcey.Cli", "#{bin}/odcey", "-m", ".", "-cc", "cc -s -O1 -flto"
+    system "ost", "to-bin", "odcey.Cli", "#{bin}/odcey", "-m", ".", "-cc", "cc -s -O1 -flto=auto"
   end
 
   test do
